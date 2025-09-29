afe5b993e1b2df18ed141d3152aa6c28c104e849
SandeebAdhikari <sa2734@njit.edu>
2025-09-29 07:27:41 -0400

    style: fixed file structure compatible to next.js

---
+++ app/(admin)/dashboard/overview/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Overview = () => {
+  return <div>page</div>;
+};
+
+export default Overview;
---
+++ app/(admin)/dashboard/staffs/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Staffs = () => {
+  return <div>page</div>;
+};
+
+export default Staffs;
