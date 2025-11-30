54d47a7d1e04a0d0b65989969593221df6226c59
SandeebAdhikari <sa2734@njit.edu>
2025-11-30 10:33:07 -0500

    fix: fixed typescript error 5

--- components/Salon/StaffLoginCard.tsx
+++ components/Salon/StaffLoginCard.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { API_ENDPOINTS } from "@/libs/api/config";
 
 type Props = {
