bd59305a0a0ac111e45385d79c4c73c454f7758b
sidradakhel <sidradakhel21@gmail.com>
2025-11-30 18:52:19 -0500

    Update global app layout with AdminHeader and spacing fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import AdminNavbar from "@/components/Admin/AdminHeader";
+
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
